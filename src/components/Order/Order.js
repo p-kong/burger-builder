112cbb77f287b4494ca2673e9357fd6646a424a5
Pauline Kong <pkong89@gmail.com>
2020-02-07 14:23:08 -0500

    testing

---
+++ src/components/Order/Order.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import classes from './Order.css';
+
+const Order = props => {
+  const ingredients = [];
+
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName],
+    });
+  }
+  console.log(ingredients);
+
+  let ingredientOutput = ingredients.map(ig => {
+    return `${ig.name} (${ig.amount}) `;
+  });
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientOutput} </p>
+      <p>Price: ${props.price.toFixed(2)}</p>
+    </div>
+  );
+};
+///testing
+
+export default Order;
